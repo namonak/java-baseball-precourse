064ab0255e0e69e657ddc080dd3e6408eb83863a
namonak <namonak@gmail.com>
2022-04-18 18:02:13 +0900

    feat : BaseballGameModel 구현 및 generateAnswer(), score() 메서드에 대한 테스트 코드 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,6 +1,68 @@
 package baseball;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
+
 public class Application {
+    static class BaseballGameModel {
+        int[] generateAnswer() {
+            int[] answer = new int[3];
+            for (int i = 0; i < answer.length; i++) {
+                answer[i] = pickNumberInRange(Utils.MIN_NUM, Utils.MAX_NUM);
+            }
+            //System.out.println("컴퓨터가 생각한 숫자는 " + answer[0] + " " + answer[1] + " " + answer[2] + " 입니다.");
+            return answer;
+        }
+
+        private int countStrike(int[] guess, int[] answer) {
+            int countStrike = 0;
+            for (int i = 0; i < answer.length; i++) {
+                if (answer[i] == guess[i]) {
+                    countStrike++;
+                }
+            }
+            return countStrike;
+        }
+
+        private int countBall(int[] guess, int[] answer) {
+            int ballCnt = 0;
+            for (int i = 0; i < guess.length; i++) {
+                if (isBall(guess[i], i, answer)) {
+                    ballCnt++;
+                }
+            }
+            return ballCnt;
+        }
+
+        private boolean isBall(int targetNum, int i, int[] answer) {
+            for (int j = 0; j < answer.length; j++) {
+                if (i == j) {
+                    continue;
+                }
+                if (targetNum == answer[j]) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        int[] score(int[] guess, int[] answer) {
+            int ballCnt = countBall(guess, answer);
+            int strikeCnt = countStrike(guess, answer);
+
+            return new int[]{ballCnt, strikeCnt};
+        }
+    }
+
+    static class BaseballGameView {}
+
+    static class BaseballGameController {}
+
+    static class Utils {
+        private static final int MIN_NUM = 1;
+        private static final int MAX_NUM = 9;
+    }
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -7,6 +7,7 @@ import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberI
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ApplicationTest extends NsTest {
     @Test
@@ -31,5 +32,99 @@ class ApplicationTest extends NsTest {
     @Override
     public void runMain() {
         Application.main(new String[]{});
+    }
+
+    @Test
+    void generateAnswerTest() {
+        Application.BaseballGameModel baseballGameModel = new Application.BaseballGameModel();
+
+        for (int i = 0; i < 100; i++) {
+            int[] result = baseballGameModel.generateAnswer();
+            assertThat(result).hasSize(3);
+            for (int j = 0; j < result.length; j++) {
+                assertThat(result[j]).isBetween(1, 9);
+            }
+        }
+    }
+
+    static class TestCase {
+        public String description;
+        public int[] guess;
+        public int[] answer;
+        public int wantBallCnt;
+        public int wantStrikeCnt;
+    }
+
+    @Test
+    void scoreTest() {
+        TestCase[] tests = new TestCase[] {
+            new TestCase() {
+                {
+                    description = "3볼";
+                    guess = new int[]{9, 1, 2};
+                    answer = new int[]{1, 2, 9};
+                    wantBallCnt = 3;
+                    wantStrikeCnt = 0;
+                }
+            },
+            new TestCase() {
+                {
+                    description = "3스트라이크";
+                    guess = new int[]{1, 2, 3};
+                    answer = new int[]{1, 2, 3};
+                    wantBallCnt = 0;
+                    wantStrikeCnt = 3;
+                }
+            },
+            new TestCase() {
+                {
+                    description = "2볼 1스트라이크";
+                    guess = new int[]{3, 8, 2};
+                    answer = new int[]{3, 2, 8};
+                    wantBallCnt = 2;
+                    wantStrikeCnt = 1;
+                }
+            },
+            new TestCase() {
+                {
+                    description = "2스트라이크";
+                    guess = new int[]{3, 8, 2};
+                    answer = new int[]{3, 8, 4};
+                    wantBallCnt = 0;
+                    wantStrikeCnt = 2;
+                }
+            },
+            new TestCase() {
+                {
+                    description = "1스트라이크";
+                    guess = new int[]{3, 8, 2};
+                    answer = new int[]{3, 9, 4};
+                    wantBallCnt = 0;
+                    wantStrikeCnt = 1;
+                }
+            },
+            new TestCase() {
+                {
+                    description = "낫싱";
+                    guess = new int[]{2, 3, 4};
+                    answer = new int[]{5, 6, 7};
+                    wantBallCnt = 0;
+                    wantStrikeCnt = 0;
+                }
+            },
+        };
+
+        for (int i = 0; i < tests.length; i++) {
+            Application.BaseballGameModel baseballGameModel = new Application.BaseballGameModel();
+
+            int[] score = baseballGameModel.score(tests[i].guess, tests[i].answer);
+
+            assertThat(score).hasSize(2);
+            int actualBallCnt = score[0];
+            int actualStrikeCnt = score[1];
+
+            assertEquals(actualBallCnt, tests[i].wantBallCnt);
+            assertEquals(actualStrikeCnt, tests[i].wantStrikeCnt);
+        }
     }
 }
